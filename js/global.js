9a7aaac70106617ed322ce80803e5dab913838d0
kan-andras <talanegyszer987@gmail.com>
2026-01-13 08:45:52 +0100

    CON-42 Login oldal szerkesztése, labelek betétele

--- js/global.js
+++ js/global.js
@@ -9,3 +9,11 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function registerbutton(){
+    const nev = document.getElementById('neve');
+    const email = document.getElementById('emailja');
+    const jelszo = document.getElementById('jelszava');
+    const jelszoagain = document.getElementById('jelszoujra');
+
+}
