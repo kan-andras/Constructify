36088300ea64e672f19d73248e95c369ec6661ad
additivepill62 <gabordaniel175@gmail.com>
2026-01-15 13:54:15 +0100

    CON-45 contact html kész (statikus, js nem funkcionális)

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+function uzenetKuldes(){
+    let veznev = document.getElementById("veznevinput").value
+    let kernev = document.getElementById("kernevinput").value
+    let email = document.getElementById("emailinput").value
+    let lakcim = document.getElementById("lakciminput").value
+    let uzenet = document.getElementById("uzenetinput").value
+    console.log(veznev, kernev, email, lakcim)
+    console.log(uzenet)
+}
