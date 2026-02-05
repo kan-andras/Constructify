20c434b5e495c0f74e2a8b23771b3bfee9798032
kan-andras <talanegyszer987@gmail.com>
2026-02-05 11:34:08 +0100

    CON-43 Gombok funkciónalitásának elkezdése javascriptben

---
+++ js/order.js
@@ -0,0 +1,25 @@
+const mainblock = document.getElementById("main-con");
+
+
+const hazgomb = document.getElementById("housebuild");
+const ceggomb = document.getElementById("factorybuild");
+const tombhazgomb = document.getElementById("arrayhousebuild");
+
+const festesgomb = document.getElementById("paintwork");
+const burkolasgomb = document.getElementById("poveringwork");
+const tapetazasgomb = document.getElementById("wallpaperingwork");
+const nyilaszarogomb = document.getElementById("shutterwork");
+const gipszkartongomb = document.getElementById("plasterboardingwork");
+const szerkezetgomb = document.getElementById("structurework");
+const falakgomb = document.getElementById("wallbreakingwork");
+const szigetelesgomb = document.getElementById("insulationwork");
+const keritesgomb = document.getElementById("fencework");
+const tetoszigetelesgomb = document.getElementById("roofinsulationwork");
+
+
+function megrendeles(){
+    mainblock.style.height = "1050px";
+    hazgomb.addEventListener("click", function() {
+        
+      });
+}
