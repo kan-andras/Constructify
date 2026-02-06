70377c5fc28f64a3d989a9115d257c0a414eaf3d
additivepill62 <gabordaniel175@gmail.com>
2026-02-06 13:25:12 +0100

    CON-51 Felhasználói profil oldal reszponzivivitásának folytatása vol2

--- js/up.js
+++ js/up.js
@@ -5,12 +5,24 @@ const D = document.getElementById('nevgomb');
 const E = document.getElementById('lakcimgomb');
 const F = document.getElementById('emailgomb');
 const G = document.getElementById('jelszogomb');
+const J = document.getElementById('telszamgomb');
+
+
+
 const A2 = document.getElementById('felnevgomb');
-const H = document.getElementById('inputmezo');
+const I = document.getElementById('nevinput');
+const I2 = document.getElementById('nevinput2');
+const I3 = document.getElementById('nevinput3');
+const I4 = document.getElementById('nevinput4');
+const I5 = document.getElementById('nevinput5');
+const I6 = document.getElementById('nevinput6');
+
+const H = document.getElementsByClassName('inputmezo');
 
 function syncHeight(){
     const height2 = A2.offsetHeight;
-    const widthA2 = A2.offsetWidth;
+    const height4 = I.offsetHeight;
+    //const widthA2 = A2.offsetWidth;
     A.style.height = height2 + 'px';
     C.style.height = height2 + 'px';
 
@@ -18,9 +30,18 @@ function syncHeight(){
     E.style.height = height2 + 'px';
     F.style.height = height2 + 'px';
     G.style.height = height2 + 'px';
-    B.style.height = height2 + 'px';/*
+    B.style.height = height2 + 'px';
+    J.style.height = height2 + 'px';
+    I.style.height = height2 + 'px';
+    I2.style.height = height4 + 'px';
+    I3.style.height = height4 + 'px';
+    I4.style.height = height4 + 'px';
+    I5.style.height = height4 + 'px';
+    I6.style.height = height4 + 'px';
+    
+    /*
     H.style.height = height2 + 'px';
-    H.style.width = widthA2*2 + 'px';*/
+    .style.width = widthA2*2 + 'px';*/
     
 }
 
