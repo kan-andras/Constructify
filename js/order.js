09e1c8e7fc6fec744831359765a37b505c68a5a2
kan-andras <talanegyszer987@gmail.com>
2026-02-12 14:12:39 +0100

    CON-43 Kiválasztott item kiírása, javascript javítása

--- js/order.js
+++ js/order.js
@@ -4,26 +4,27 @@ const mainblock = document.getElementById("main-con");
 //const hazgomb = document.getElementById("housebuild");
 //const ceggomb = document.getElementById("factorybuild");
 //const tombhazgomb = document.getElementById("arrayhousebuild");
-//
-//const hazimg = document.getElementById("house");
-//const cegimg = document.getElementById("factory");
-//const tömbimg = document.getElementById("array");
-//
-//const festesgomb = document.getElementById("paintwork");
-//const burkolasgomb = document.getElementById("poveringwork");
-//const tapetazasgomb = document.getElementById("wallpaperingwork");
-//const nyilaszarogomb = document.getElementById("shutterwork");
-//const gipszkartongomb = document.getElementById("plasterboardingwork");
-//const szerkezetgomb = document.getElementById("structurework");
-//const falakgomb = document.getElementById("wallbreakingwork");
-//const szigetelesgomb = document.getElementById("insulationwork");
-//const keritesgomb = document.getElementById("fencework");
-//const tetoszigetelesgomb = document.getElementById("roofinsulationwork");
+
+const hazimg = document.getElementById("house");
+const cegimg = document.getElementById("factory");
+const tömbimg = document.getElementById("array");
+
+const festesgomb = document.getElementById("paintwork");
+const burkolasgomb = document.getElementById("poveringwork");
+const tapetazasgomb = document.getElementById("wallpaperingwork");
+const nyilaszarogomb = document.getElementById("shutterwork");
+const gipszkartongomb = document.getElementById("plasterboardingwork");
+const szerkezetgomb = document.getElementById("structurework");
+const falakgomb = document.getElementById("wallbreakingwork");
+const szigetelesgomb = document.getElementById("insulationwork");
+const keritesgomb = document.getElementById("fencework");
+const tetoszigetelesgomb = document.getElementById("roofinsulationwork");
 
 const gombok = document.querySelectorAll(".buttons");
 const jobbgombok = document.querySelectorAll(".rightbuttons");
 const kepek = document.querySelectorAll(".image");
 const jobbkepek = document.querySelectorAll(".rightimage");
+
 var seged = false;
 var segedsecond = false;
 
@@ -36,30 +37,34 @@ function megrendeles(){
         gomb.addEventListener("click", () => {
           // glow levétele mindről
           kepek.forEach(kep => kep.classList.remove("glow"));
-          
+          jobbkepek.forEach(kep => kep.classList.remove("glowtwo"));
           // glow hozzáadása csak az aktuálishoz
           kepek[index].classList.add("glow");
           seged = true;
-          mainblock.style.height = "1150px";
+          mainblock.style.height = "1080px";
         });
       });
     }
 
 
     if (seged){
-      jobbkepek.forEach(kep => kep.classList.remove("glow"));
+      jobbkepek.forEach(kep => kep.classList.remove("glowtwo"));
     }
     else{
       jobbgombok.forEach((jobbgomb, index) => {
         jobbgomb.addEventListener("click", () => {
           // glow levétele mindről
-          jobbkepek.forEach(kep => kep.classList.remove("glow"));
-          
+          jobbkepek.forEach(kep => kep.classList.remove("glowtwo"));
+          kepek.forEach(kep => kep.classList.remove("glow"));
           // glow hozzáadása csak az aktuálishoz
-          jobbkepek[index].classList.add("glow");
+          jobbkepek[index].classList.add("glowtwo");
           segedsecond = true;
-          mainblock.style.height = "1150px";
+          mainblock.style.height = "1080px";
         });
       });
     }
+
+    festesgomb.addEventListener("click", () =>{
+      let kivalasztottkep = document.getElementById("selecteditem").innerText = "valami";
+    });
 }
