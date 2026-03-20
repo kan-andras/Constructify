14e4d32b1629798963ba294ece4fe63498a5a2b9
additivepill62 <gabordaniel175@gmail.com>
2026-03-20 13:25:35 +0100

    CON-53 login oldal profilrendszerének localstorageba való ávitele vol1

--- js/login.js
+++ js/login.js
@@ -2,10 +2,12 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
-let userMap = new Map([["name", "John Doe"],[ "email", "johndoedoesthings@gmail.com"],[ "pw", "johndoeisthebest@1"]]);
-userMap.forEach((value, key) => console.log(`${key} : ${value}`));
+let userMap = new Map([
+    ["name", "John Doe"],[ "email", "johndoedoesthings@gmail.com"],[ "pw", "johndoeisthebest@1"],
+    ["name", "Jane Doe"],[ "email", "janedoedoesnotdothings@gmail.com"],[ "pw", "bashbashasdasdsad@2"]
 
-
+]);
+//userMap.forEach((value, key) => console.log(`${key} : ${value}`));
 
 
 function loginHTML(){
@@ -15,6 +17,70 @@ function loginHTML(){
 
 
 }
+
+
+
+// 1. Több adat mentése
+function adatMentesOlvasas() {
+    const users = [
+        { name: "John Doe", email: "johndoedoesthings@gmail.com" , pw : "johndoeisthebest@1"},
+        { name: "Jane Doe", email: "janedoedoesnotdothings@gmail.com" , pw : "bashbashasdasdsad@2"},
+        { name: "Jane Roe", email: "janeroe@gmail.com" , pw: "bvankasc]<@335"}
+      ];
+      
+      localStorage.setItem("users", JSON.stringify(users));
+  // 2. Kiolvasás
+  const savedUsers = JSON.parse(localStorage.getItem("users")) || [];
+  
+  
+  // 3. Egy konkrét adat kiválasztása
+  const selectedUser = savedUsers.find(user => user.email === "johndoedoesthings@gmail.com");
+  
+  
+  // 4. Használat
+  new Promise((resolve, reject) => {
+    if (selectedUser) {
+        resolve("un: "/*
+        selectedUser.name+ "email: "+
+        selectedUser.email+ "pw: "+
+        selectedUser.pw */) 
+      } else {
+        reject("Nincs ilyen user")
+      }
+  })
+  
+}
+
+    adatMentesOlvasas().then(uzenet=> console.log(uzenet)).catch(hiba => console.log(hiba))
+
+
+
+/*
+let users = [
+    { name: "Dani", age: 18 },
+    { name: "John Doe", age: 32 }
+  ];
+  
+  localStorage.setItem("users", JSON.stringify(users));
+  let users2 = JSON.parse(localStorage.getItem("users"));
+
+  console.log(users);*/
+/*
+const user = {
+    name: "Dani",
+    age: 18,
+    name: "John Doe",
+    age:32
+  };
+  
+  localStorage.setItem("user", JSON.stringify(user));
+  const user2 = JSON.parse(localStorage.getItem("user"));
+
+  console.log(user2.name);*/
+
+
+
+
 
 function loginFunc(){
     let logEmail = document.getElementById('loginEmail').value;
@@ -37,6 +103,9 @@ function loginFunc(){
                 resolve("bejelentkezés")
             }        
     })
+    
+  
+    
 }
 
 
