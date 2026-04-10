13880967fb1c6f491ab4ed9acb3d09118501c36a
OkronaGergo1 <126560934+OkronaGergo1@users.noreply.github.com>
2026-04-10 15:36:56 +0200

    CON-47 GYIK oldal nézetének kialakítása és reszponzivitásának elkészítése

--- js/gyik.js
+++ js/gyik.js
@@ -1,8 +1,19 @@
 const prob = document.querySelectorAll(".prob");
- 
+
+prob.forEach(problem => {
+    const nyil = problem.querySelector(".kep");
+    nyil.src = "../images/Arrow_down.png";
+});
+
 prob.forEach(problem => {
   problem.addEventListener("click", () => {
     const sz = problem.nextElementSibling;
     sz.classList.toggle("active");
+    const nyil = problem.querySelector(".kep");
+    if (sz.classList.contains("active")) {
+      nyil.src = "../images/Arrow_up.png";
+    } else {
+      nyil.src = "../images/Arrow_down.png";
+    }
   });
 });
